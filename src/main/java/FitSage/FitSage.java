c1110eacb24fb527300ec65dafe0e69cac3c5d92
jaskaran singh <jaskaran4323@gmail.com>
2025-08-10 13:28:58 -0400

    Add FitSage main application class

---
+++ src/main/java/FitSage/FitSage.java
@@ -0,0 +1,13 @@
+package fitsage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitSage {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitSage.class, args);
+	}
+
+}
